4b950ed1e5a38ad35477e28bfa3513656198cb35
Sardar <sardarbekatabekov@gmail.com>
2022-08-03 11:43:38 +0600

    update site

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import toDo from "./assets/img/to-do.png";
 import sbBar from "./assets/img/sbBar.png";
 import neobis from "./assets/img/neobis.png";
 import navidu from "./assets/img/navidu.png";
+import NeoCafe from "./assets/img/NeoCafe.png";
 import zigmund from "./assets/img/zigmund.jpg";
 import aliDent from "./assets/img/aliDent.png";
 import crmCafe from "./assets/img/crm-cafe.jpg";
@@ -19,23 +20,39 @@ import yogaLanding from "./assets/img/yoga_landing.png";
 function App() {
   return (
     <div className="app">
-      <header className="app-header">
-        <div className="container">
-          {/* <div className="header-inner">
-            <div className="logo">Logo</div>
-            <nav id="nav" className="nav">
-              <button className="btn" id="login">
-                Sign In
-              </button>
-            </nav>
-          </div> */}
-        </div>
-      </header>
       <main class="portfolio">
         <div class="container">
           <h2 class="section-title">Портфолио</h2>
           <h2>Коммерческие проекты</h2>
           <div id="portfolioInner" class="portfolio-inner">
+            <a
+              href="https://match.zigmund.online/"
+              target="_blank"
+              rel="noreferrer"
+              class="card-item"
+              data-work-id="605b843eeef00014b0ea18f2"
+            >
+              <div class="categories-wrapper">
+                {/* <span class="tech-name from-psd" data-teck-name="fromPsd">
+                  From Stydi
+                </span> */}
+                <span class="tech-name react" data-teck-name="html">
+                  React
+                </span>
+                <span class="tech-name redux" data-teck-name="css">
+                  Redux
+                </span>
+                <span class="tech-name type-script" data-teck-name="css">
+                  TypeScript
+                </span>
+              </div>
+              <div class="card-image-overlay">
+                <img src={zigmund} alt="zigmund" class="card-image" />
+              </div>
+              <p class="card-name">
+                <span>Подбор психолога</span>
+              </p>
+            </a>
             <a
               href="https://neobis.kg/"
               target="_blank"
@@ -86,34 +103,7 @@ function App() {
                 <span>Доставка еды (+Админ панель)</span>
               </p>
             </a>
-            <a
-              href="https://match.zigmund.online/"
-              target="_blank"
-              rel="noreferrer"
-              class="card-item"
-              data-work-id="605b843eeef00014b0ea18f2"
-            >
-              <div class="categories-wrapper">
-                {/* <span class="tech-name from-psd" data-teck-name="fromPsd">
-                  From Stydi
-                </span> */}
-                <span class="tech-name react" data-teck-name="html">
-                  React
-                </span>
-                <span class="tech-name redux" data-teck-name="css">
-                  Redux
-                </span>
-                <span class="tech-name type-script" data-teck-name="css">
-                  TypeScript
-                </span>
-              </div>
-              <div class="card-image-overlay">
-                <img src={zigmund} alt="zigmund" class="card-image" />
-              </div>
-              <p class="card-name">
-                <span>Подбор психолога</span>
-              </p>
-            </a>
+
             <a
               href="https://postexpress.org/"
               target="_blank"
@@ -134,10 +124,16 @@ function App() {
               </div>
               <p class="card-name">
                 <span>
-                  Логистика грузов (возможно, не работает в вашей стране)
+                  Логистика грузов
                 </span>
               </p>
             </a>
+
+          </div>
+        </div>
+        <div class="container">
+          <h2>Отмененные коммерческие проекты</h2>
+          <div id="portfolioInner" class="portfolio-inner">
             <a
               href="http://hub.neobis.kg/"
               target="_blank"
@@ -161,7 +157,7 @@ function App() {
               </div>
               <p class="card-name">
                 <span>
-                  Сompany accounting (in progress, hubadmin@gmail.com,
+                  Сompany accounting ( hubadmin@gmail.com,
                   Superpasswordhub2)
                 </span>
               </p>
@@ -169,7 +165,7 @@ function App() {
           </div>
         </div>
         <div class="container">
-          <h3>Проекты для внутреннего пользования компании</h3>
+          <h2>Проекты для внутреннего пользования компании</h2>
           <div id="portfolioInner" class="portfolio-inner">
             <div class="card-item" data-work-id="605b843eeef00014b0ea18f2">
               <div class="categories-wrapper">
@@ -253,7 +249,7 @@ function App() {
           </div>
         </div>
         <div class="container">
-          <h3>Недоступные или закрытые проекты</h3>
+          <h2>Недоступные или закрытые проекты</h2>
           <div id="portfolioInner" class="portfolio-inner">
             <div class="card-item" data-work-id="605b843eeef00014b0ea18f2">
               <div class="categories-wrapper">
@@ -293,13 +289,14 @@ function App() {
                 <span>Мобильное приложение для грузоперевозчиков</span>
               </p>
             </div>
+
           </div>
         </div>
         <div class="container">
-          <h2>Некоммерческие проекты</h2>
+          <h2>Тестовые задании и учебные проекты</h2>
           <div id="portfolioInner" class="portfolio-inner">
             <a
-              href="https://miniblogsite.herokuapp.com/"
+              href="https://boiling-dusk-38246.herokuapp.com/"
               target="_blank"
               rel="noreferrer"
               class="card-item"
@@ -311,6 +308,26 @@ function App() {
                 </span>
                 <span class="tech-name react" data-teck-name="css">
                   React
+                </span>
+              </div>
+              <div class="card-image-overlay">
+                <img src={NeoCafe} alt="onlineShop" class="card-image" />
+              </div>
+              <p class="card-name">
+                <span>CRM система для управление кофейней</span>
+              </p>
+            </a>
+
+            <a
+              href="https://miniblogsite.herokuapp.com/"
+              target="_blank"
+              rel="noreferrer"
+              class="card-item"
+              data-work-id="605b843eeef00014b0ea18f2"
+            >
+              <div class="categories-wrapper">
+                <span class="tech-name js" data-teck-name="js">
+                  JavaScript
                 </span>
               </div>
               <div class="card-image-overlay">
@@ -329,7 +346,7 @@ function App() {
               data-work-id="605b843eeef00014b0ea18f2"
             >
               <div class="categories-wrapper">
-                <span class="tech-name css" data-teck-name="html">
+                <span class="tech-name css" data-teck-name="css">
                   CSS
                 </span>
                 <span class="tech-name react" data-teck-name="css">
